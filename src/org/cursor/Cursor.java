18f5f8445e323ef4a0a866bd7f905d19b6fdcbdb
KanishkarU <kanishkar1705@gmail.com>
2022-03-29 18:16:52 +0530

    29/03/2022

---
+++ src/org/cursor/Cursor.java
@@ -0,0 +1,30 @@
+package org.cursor;
+
+import java.util.*;
+
+
+public class Cursor {
+	public static void main(String[] args) {
+		
+		//Enumeration
+	
+		
+			Vector<Integer> v = new Vector<>();
+			v.add(1);
+			v.add(2);
+			v.add(3);
+			
+			
+			Enumeration<Integer> elements2 = v.elements();
+			while(elements2.hasMoreElements()) {
+				Integer l = elements2.nextElement();
+				System.out.println(l);
+			}
+		}
+		
+		
+		
+		
+	}
+
+
